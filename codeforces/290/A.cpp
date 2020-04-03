22008ddd6184f26c9d8988b7bae8302998fcf67f
Mahmudul Ahsan Mahi <mamahi1998@gmail.com>
2020-04-03 19:16:00 +0600

    Add solution for problem `A - Mysterious strings` Link: https://codeforces.com/contest/290/problem/A Tags: *special problem, implementation, *1400 Ref: https://codeforces.com/contest/290/submission/75346550

---
+++ codeforces/290/A.cpp
@@ -0,0 +1,210 @@
+#include <stdio.h>
+int main()
+
+{
+    int n;
+    scanf("%d",&n);
+    
+    if(n==1)
+    {
+        printf("Washington");
+    }
+    
+    else if(n==2)
+    {
+        printf("Adams");
+    }
+    
+     else if(n==3)
+    {
+        printf("Jefferson");
+    }
+    
+     else if(n==4)
+    {
+        printf("Madison");
+    }
+    
+     else if(n==5)
+    {
+        printf("Monroe");
+    }
+    
+     else if(n==6)
+    {
+        printf("Adams");
+    }
+    
+     else if(n==7)
+    {
+        printf("Jackson");
+    }
+    
+     else if(n==8)
+    {
+        printf("Van Buren");
+    }
+    
+     else if(n==9)
+    {
+        printf("Harrison");
+    }
+    
+     else if(n==10)
+    {
+        printf("Tyler");
+    }
+    
+     else if(n==11)
+    {
+        printf("Polk");
+    }
+    
+     else if(n==12)
+    {
+        printf("Taylor");
+    }
+    
+     else if(n==13)
+    {
+        printf("Fillmore");
+    }
+    
+     else if(n==14)
+    {
+        printf("Pierce");
+    }
+    
+     else if(n==15)
+    {
+        printf("Buchanan");
+    }
+    
+     else if(n==16)
+    {
+        printf("Lincoln");
+    }
+    
+     else if(n==17)
+    {
+        printf("Johnson");
+    }
+    
+     else if(n==18)
+    {
+        printf("Grant");
+    }
+    
+     else if(n==19)
+    {
+        printf("Hayes");
+    }
+    
+     else if(n==20)
+    {
+        printf("Garfield");
+    }
+    
+     else if(n==21)
+    {
+        printf("Arthur");
+    }
+    
+     else if(n==22)
+    {
+        printf("Cleveland");
+    }
+    
+     else if(n==23)
+    {
+        printf("Harrison");
+    }
+    
+     else if(n==24)
+    {
+        printf("Cleveland");
+    }
+    
+     else if(n==25)
+    {
+        printf("McKinley");
+    }
+    
+     else if(n==26)
+    {
+        printf("Roosevelt");
+    }
+    
+     else if(n==27)
+    {
+        printf("Taft");
+    }
+    
+     else if(n==28)
+    {
+        printf("Wilson");
+    }
+    
+     else if(n==29)
+    {
+        printf("Harding");
+    }
+    
+     else if(n==30)
+    {
+        printf("Coolidge");
+    }
+    
+     else if(n==31)
+    {
+        printf("Hoover");
+    }
+    
+     else if(n==32)
+    {
+        printf("Roosevelt");
+    }
+    
+     else if(n==33)
+    {
+        printf("Truman");
+    }
+    
+     else if(n==34)
+    {
+        printf("Eisenhower");
+    }
+    
+     else if(n==35)
+    {
+        printf("Kennedy");
+    }
+    
+     else if(n==36)
+    {
+        printf("Johnson");
+    }
+    
+     else if(n==37)
+    {
+        printf("Nixon");
+    }
+    
+     else if(n==38)
+    {
+        printf("Ford");
+    }
+    
+     else if(n==39)
+    {
+        printf("Carter");
+    }
+    
+     else if(n==40)
+    {
+        printf("Reagan");
+    }
+    
+    
+    return 0;
+}
