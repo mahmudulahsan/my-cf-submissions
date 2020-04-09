5db16fd4c275234752eb22cf6c748683f4789585
Mahmudul Ahsan Mahi <mamahi1998@gmail.com>
2020-04-09 13:31:00 +0600

    Add solution for problem `A - Maximum in Table` Link: https://codeforces.com/contest/509/problem/A Tags: brute force, implementation, *800 Ref: https://codeforces.com/contest/509/submission/75963408

---
+++ codeforces/509/A.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, i, j, a[10][10];
+
+    cin>>n;
+
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            a[i][0]=1;
+            a[0][j]=1;
+        }
+    }
+    for(i=1;i<n;i++)
+    {
+        for(j=1;j<n;j++)
+        {
+            a[i][j]=a[i-1][j]+a[i][j-1];
+        }
+    }
+
+    cout<<a[n-1][n-1]<<endl;
+
+
+}
