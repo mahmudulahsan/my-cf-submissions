f8ca248a4a9894538b4f215a29a731e547ee1d1a
Mahmudul Ahsan Mahi <mamahi1998@gmail.com>
2020-04-09 14:10:00 +0600

    Add solution for problem `A - Game` Link: https://codeforces.com/contest/513/problem/A Tags: constructive algorithms, math, *800 Ref: https://codeforces.com/contest/513/submission/75966885

---
+++ codeforces/513/A.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2, k1, k2;
+
+    cin>>n1>>n2>>k1>>k2;
+
+    int f=(n1>n2)?1:0;
+
+    if(f==1)
+        cout<<"First"<<endl;
+    if(f==0)
+        cout<<"Second"<<endl;
+
+
+
+
+}
