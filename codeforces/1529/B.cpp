9a2421f409fc4148a928e5585f29bc001f2a015a
Mahmudul Ahsan Mahi <mamahi1998@gmail.com>
2021-05-25 22:13:00 +0600

    Add solution for problem `B - Sifid and Strange Subsequences` Link: https://codeforces.com/contest/1529/problem/B Tags: greedy, math, sortings, *1100 Ref: https://codeforces.com/contest/1529/submission/117341196

---
+++ codeforces/1529/B.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define mod       1e9+7
+#define pi        acos(-1)
+#define gcd(a,b)  __gcd(a,b)
+#define lcm(a,b)  (a/__gcd(a,b))*b
+#define ll        long long
+#define pb        push_back
+#define endl      '\n'
+//#define ios       ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+
+
+int main()
+{
+    //ios
+
+    int t; cin>>t;
+
+    while(t--){
+        int n; cin>>n;
+
+        int a[n];
+
+        int c=0, mx = INT_MAX;
+
+        for(int i=0;i<n;i++) cin>>a[i];
+
+        sort(a, a+n);
+
+        for(int i=1;i<n;i++){
+            mx=min(mx, abs(a[i]-a[i-1]));
+            if(mx<a[i]) break;
+            c++;
+        }
+        cout<<c+1<<endl;
+    }
+
+
+
+    //cout<<"Case "<<i++<<":"<<" "<<sz<<endl;
+
+    return 0;
+}
