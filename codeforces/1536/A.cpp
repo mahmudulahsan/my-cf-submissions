a68f47189c5ba8b5bda478d8c7ef26f55f2c33c0
Mahmudul Ahsan Mahi <mamahi1998@gmail.com>
2021-06-06 21:34:00 +0600

    Add solution for problem `A - Omkar and Bad Story` Link: https://codeforces.com/contest/1536/problem/A Tags: brute force, constructive algorithms, *800 Ref: https://codeforces.com/contest/1536/submission/118629750

---
+++ codeforces/1536/A.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define mod       1e9+7
+#define pi        acos(-1)
+#define gcd(a,b)  __gcd(a,b)
+#define lcm(a,b)  (a/__gcd(a,b))*b
+#define ll        long long
+#define pb        push_back
+#define endl      '\n'
+#define yes       cout<<"YES"<<endl
+#define no        cout<<"NO"<<endl
+//#define ios       ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+    //ios
+
+    int t; cin>>t;
+    while(t--){
+        ll n; cin>>n;
+        ll a[n];
+        ll m=0;
+        for(ll i=0;i<n;i++){
+           cin>>a[i];
+           if(a[i]<0) m=1;
+        }
+        if(m==1){
+            no;
+        }
+
+        else{
+            sort(a, a+n);
+            ll mx=a[n-1];
+            set<ll>s;
+            for(ll i=0;i<=mx;i++){
+                s.insert(i);
+            }
+            yes;
+            cout<<mx+1<<endl;
+
+            for(auto u: s) cout<<u<<" ";
+            cout<<endl;
+        }
+    }
+
+
+    //cout<<"Case "<<i++<<":"<<" "<<sz<<endl;
+
+    return 0;
+}
