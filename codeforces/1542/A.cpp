7efc1dbf5827f6f58d892316e16dcf98cd760d84
Mahmudul Ahsan Mahi <mamahi1998@gmail.com>
2021-07-03 19:42:00 +0600

    Add solution for problem `A - Odd Set` Link: https://codeforces.com/contest/1542/problem/A Tags: math, *800 Ref: https://codeforces.com/contest/1542/submission/121215210

---
+++ codeforces/1542/A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define mod       1e9+7
+#define pi        acos(-1)
+#define gcd(a,b)  __gcd(a,b)
+#define lcm(a,b)  (a/__gcd(a,b))*b
+#define ll        long long
+#define pb        push_back
+#define endl      '\n'
+#define yes       cout<<"YES"<<endl
+#define no        cout<<"NO"<<endl
+#define ios       ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+
+
+int main()
+{
+    //ios
+
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        int a[2*n];
+        int e = 0, o =0;
+        for(int i=0;i<2*n;i++){
+            cin>>a[i];
+            if(a[i]%2!=0) o++;
+            else e++;
+        }
+        if(o==e) cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+
+    }
+
+    //cout<<"Case "<<i++<<":"<<" "<<sz<<endl;
+
+
+    return 0;
+}
