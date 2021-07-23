544011c408dd6b28b5afa43a343ebe89a1b7fce4
Mahmudul Ahsan Mahi <mamahi1998@gmail.com>
2021-07-23 20:49:00 +0600

    Add solution for problem `A - Polycarp and Coins` Link: https://codeforces.com/contest/1551/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/1551/submission/123460911

---
+++ codeforces/1551/A.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define mod       1e9+7
+#define pi        acos(-1)
+#define gcd(a,b)  __gcd(a,b)
+#define lcm(a,b)  (a/__gcd(a,b))*b
+#define ll        long long
+#define pb        push_back
+#define endl      '\n'
+#define yes       cout<<"YES"<<endl
+#define no        cout<<"NO"<<endl
+#define ios       ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+    //ios
+
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        int a = ceil(n/3.0);
+        int b = (n-a)/2;
+        int ans = a*1+b*2;
+        if(ans==n){
+            cout<<a<<" "<<b<<endl;
+        }
+        else cout<<b<<" "<<a<<endl;
+    }
+
+
+
+    //cout<<"Case "<<i++<<":"<<" "<<sz<<endl;
+
+
+    return 0;
+}
