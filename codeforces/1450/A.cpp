10432284dab7be5e3239b76a4d8ac5f793dce7af
Mahmudul Ahsan Mahi <mamahi1998@gmail.com>
2021-07-24 10:50:00 +0600

    Add solution for problem `A - Avoid Trygub` Link: https://codeforces.com/contest/1450/problem/A Tags: constructive algorithms, sortings, *800 Ref: https://codeforces.com/contest/1450/submission/123569571

---
+++ codeforces/1450/A.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define mod       1e9+7
+#define pi        acos(-1)
+#define gcd(a,b)  __gcd(a,b)
+#define lcm(a,b)  (a/__gcd(a,b))*b
+#define ll        long long
+#define pb        push_back
+#define endl      '\n'
+#define yes       cout<<"YES"<<endl
+#define no        cout<<"NO"<<endl
+#define ios       ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+    //ios
+
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        string s; cin>>s;
+        sort(s.begin(), s.end());
+        cout<<s<<endl;
+    }
+
+
+
+    //cout<<"Case "<<i++<<":"<<" "<<sz<<endl;
+
+
+    return 0;
+}
