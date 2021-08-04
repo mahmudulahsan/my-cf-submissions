6080be4599e9898c4adf9a6cc68301a8fa5aadfc
Mahmudul Ahsan Mahi <mamahi1998@gmail.com>
2021-08-05 02:13:00 +0600

    Add solution for problem `A - Stickers and Toys` Link: https://codeforces.com/contest/1187/problem/A Tags: math, *900 Ref: https://codeforces.com/contest/1187/submission/124899039

---
+++ codeforces/1187/A.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define mod       1e9+7
+#define pi        acos(-1)
+#define gcd(a,b)  __gcd(a,b)
+#define lcm(a,b)  (a/__gcd(a,b))*b
+#define ll        long long
+#define pb        push_back
+#define m_p       make_pair
+#define endl      '\n'
+#define yes       cout<<"YES"<<endl
+#define no        cout<<"NO"<<endl
+#define gap       " "
+#define flash     ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+    //flash
+
+    int tc; cin>>tc;
+    while(tc--){
+        int n, s, t; cin>>n>>s>>t;
+        int mx = max(s, t);
+        int mn = min(s, t);
+
+        cout<<n-mn+1<<endl;
+    }
+
+    //cout<<"Case "<<i++<<":"<<" "<<sz<<endl;
+
+    return 0;
+}
